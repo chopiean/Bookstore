df4e688709afa70bbe73aee875c05c989628da89
anle10 <anle10@myy.haaga-helia.fi>
2025-09-09 23:30:49 +0300

    Bookstore: Crud

--- src/main/java/fi/haagahelia/bookstore/controller/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/controller/BookController.java
@@ -1,9 +1,15 @@
 package fi.haagahelia.bookstore.controller;
 
+import fi.haagahelia.bookstore.model.Book;
 import fi.haagahelia.bookstore.repository.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 public class BookController {
@@ -17,6 +23,26 @@ public class BookController {
     @GetMapping("/booklist")
     public String showBookList(Model model) {
         model.addAttribute("books", repo.findAll());
-        return "booklist"; // -> templates/booklist.html
+        return "booklist"; 
     }
+
+    @GetMapping("/addbook")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(@ModelAttribute Book book) {
+       repo.save(book);
+       return "redirect:/booklist";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        repo.deleteById(id);
+        return "redirect:/booklist";
+    }
+    
+    
 }
