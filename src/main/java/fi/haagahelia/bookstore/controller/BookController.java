4b3808cef0cc0844865ce6608176699990603ace
anle10 <anle10@myy.haaga-helia.fi>
2025-09-09 10:27:42 +0300

    create Bookstore: Listpage

--- src/main/java/fi/haagahelia/bookstore/controller/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/controller/BookController.java
@@ -1,6 +1,6 @@
 package fi.haagahelia.bookstore.controller;
 
-import fi.haagahelia.bookstore.model.Book;
+import fi.haagahelia.bookstore.repository.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController {
 
-    @GetMapping("/index")
-    public String index(Model model) {
-        // Example book to pass to the view
-        Book book = new Book("Effective Java", "Joshua Bloch", 2018, "9780134685991", 45.0);
-        model.addAttribute("book", book);
-        return "booklist";
+    private final BookRepository repo;
+
+    public BookController(BookRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/booklist")
+    public String showBookList(Model model) {
+        model.addAttribute("books", repo.findAll());
+        return "booklist"; // -> templates/booklist.html
     }
 }
