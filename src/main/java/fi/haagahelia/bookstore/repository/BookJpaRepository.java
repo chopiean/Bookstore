fb9efb2604e2febe90c4cce546bca6cf6d3008e7
anle10 <anle10@myy.haaga-helia.fi>
2025-09-22 21:46:15 +0300

    Add release.txt with Github release address

--- src/main/java/fi/haagahelia/bookstore/repository/BookJpaRepository.java
+++ src/main/java/fi/haagahelia/bookstore/repository/BookJpaRepository.java
@@ -1,5 +1,10 @@
 package fi.haagahelia.bookstore.repository;
 
-public class BookJpaRepository {
+import fi.haagahelia.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;;
+
+@RepositoryRestResource(path = "books")
+public interface BookJpaRepository extends JpaRepository<Book, Long> {
 
 }
